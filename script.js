c7ffc19886cb843b5b8c57c53d4d4093e33911bb
AldinaBrkic <142264576+AldinaBrkic@users.noreply.github.com>
2023-08-24 01:57:29 -0700

    Update script.js
    The previous function could assign field colors, but I couldn't clear the colors of the last clicked field, so I added an ID Undo button to keep track of which field was clicked last. I also added a lastClickedSquare variable that keeps track of which square was clicked.

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-var counter = 0;
 var el1 = document.getElementById ("elNo1");
 var el2 = document.getElementById ("elNo2");
 var el3 = document.getElementById ("elNo3");
@@ -8,24 +7,36 @@ var el6 = document.getElementById ("elNo6");
 var el7 = document.getElementById ("elNo7");
 var el8 = document.getElementById ("elNo8");
 var el9 = document.getElementById ("elNo9");
-function elementt(allElements)
-{
-	var bigElement = document.getElementById(allElements);
-	if ( bigElement.style.backgroundColor == "" )
-	{
-		counter++;
-		if (counter == 1)
-		{
-			bigElement.style.backgroundColor = "red";
-			
-		}
-		
-		if (counter == 2)
-		{
-			bigElement.style.backgroundColor = "blue";
-			counter = 0;
-		}
-	}
+var counter = 0;
+let lastClickedSquare = null;
+
+
+function elementt(allElements) {
+  const squares = document.querySelectorAll('.el_No');
+  const undoButton = document.getElementById('undoButton');
+
+
+
+  squares.forEach(square => {
+    square.addEventListener('click', () => {
+      if (square.style.backgroundColor === "") {
+        if (lastClickedSquare && lastClickedSquare.style.backgroundColor === "red") {
+          square.style.backgroundColor = "blue";
+          lastClickedSquare = square;
+        } else {
+          square.style.backgroundColor = "red";
+          lastClickedSquare = square;
+        }
+      }
+    });
+  });
+
+  undoButton.addEventListener('click', () => {
+    if (lastClickedSquare) {
+      lastClickedSquare.style.backgroundColor = '';
+      lastClickedSquare = null;
+    }
+  });
 }
 
 
